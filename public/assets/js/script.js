c2c9b3f2f49dfa81686be19a3b702eae8deff605
CaroTrd <carolinatirado92@gmail.com>
2020-03-18 02:14:42 +0100

    add boutton share facebook

---
+++ public/assets/js/script.js
@@ -0,0 +1,40 @@
+// Google Tag Manager
+
+(function(w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({
+        "gtm.start": new Date().getTime(),
+        event: "gtm.js"
+    });
+    var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != "dataLayer" ? "&l=" + l : "";
+    j.async = true;
+    j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+    f.parentNode.insertBefore(j, f);
+})(window, document, "script", "dataLayer", "GTM-TB48ZGS");
+
+// Global site tag (gtag.js) - Google Analytics
+
+document.createTextNode(
+    "<script async src='https://www.googletagmanager.com/gtag/js?id=UA-160690896-1' />"
+);
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "UA-160690896-1");
+
+
+// facebook share
+
+/*(function(d, s, id) {
+  console.log(d, s, id)
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));*/
